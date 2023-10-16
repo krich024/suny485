5ac7f54d85a2baadb69daa31ea791e650f5ac19f
krich024 <145486595+krich024@users.noreply.github.com>
2023-10-16 15:36:21 -0400

    Test Code for Homework 10 (fruit_query.py)
    suny485/projects/hw10/fruit_query.py

--- suny485/tests/hw10/test_hw10.py
+++ suny485/tests/hw10/test_hw10.py
@@ -2,9 +2,9 @@ import pytest
 from suny485.projects.hw10.fruit_query import is_it_a_fruit
 
 """
-Checking to make sure that apple appears in the list
-Have a test case that doesn't have a fruit name in the code and check if it can return False
-Since the list is made of strings only, there shouldn't be tuples so I can create a test file providing that there's no tuples
+Test cases for the types of inputs that are allowed to be added
+Identifying what could happen if we use different list types
+If I add in the wrong input, a error should appear.
 """
 
 class TestFruitHappy(object):
@@ -21,4 +21,22 @@ class TestFruitTuples(object):
     def test_tuples_fruit(self):
         assert is_it_a_fruit(("apple", "grape")) == False
 
+
+class TestFruitSet(object):
+    def test_set_fruit(self):
+        assert is_it_a_fruit({"apple", "pear"}) == False
+
+
+class TestFruitBad(object):
+    def test_bad_fruit(self):
+        assert is_it_a_fruit('red') == False
+
+class TestFruitGood(object):
+    def test_good_fruit(self):
+        assert is_it_a_fruit('grape') == True
+
+
+class TestFruitInt(object):
+    def test_fruit_int(self):
+        assert is_it_a_fruit('3') == False
 
