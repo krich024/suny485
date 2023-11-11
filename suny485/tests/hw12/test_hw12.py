b48984322a7b291e7fc8334fe55bf5539900d28c
krich024 <145486595+krich024@users.noreply.github.com>
2023-11-10 20:52:45 -0500

    Parametrize Test Code Hw 12 (hw12_code.py) 5
    suny485/tests/hw12/test_hw12.py
    + Edited Code

--- suny485/tests/hw12/test_hw12.py
+++ suny485/tests/hw12/test_hw12.py
@@ -10,60 +10,50 @@ happy path:
 unhappy path:
     - inputs:
         - non char that's not in the list return a float value
-        - char in the list returns wrong value (wrong float or non float)
+        - using complexifiers for words
         - empty str
     - outputs:
-        - returns 0.0
-        - 50.0 if non char are added to char complexifiers
+        - returns floats that are < 100.0
 """
 
-@pytest.mark.parametrize('happy_char', [('~@', 100.0)])
 
+@pytest.mark.parametrize('happy_char', [('~@', 100.0)])
 def test_input_happy(happy_char):
     char, num_complexifiers = happy_char
     happy_char = compute_complexity(char)
     assert happy_char == num_complexifiers
 
 
-@pytest.mark.parametrize('non_char', [('1', 0.0)])
-
-def test_nonchar_input(non_char):
-    char, num_complexifiers = non_char
+@pytest.mark.parametrize('int', [('1', 0.0)])
+def test_nonchar_input(int):
+    char, num_complexifiers = int
     non_char = compute_complexity(char)
     assert non_char == num_complexifiers
 
 
-@pytest.mark.parametrize('unhappy_char', [('#&_', 25.0)])
-
-def test_unhappy_input(unhappy_char):
-    char, num_complexifiers = unhappy_char
+@pytest.mark.parametrize('char_spelled', [('n@me', 25.0)])
+def test_unhappy_input(char_spelled):
+    char, num_complexifiers = char_spelled
     unhappy_char = compute_complexity(char)
-    assert unhappy_char != num_complexifiers
+    assert unhappy_char == num_complexifiers
 
 
-@pytest.mark.parametrize('empty_list', [([''], 0.0)])
-
-def test_empty_list(empty_list):
-    non_char, num_complexifiers = empty_list
+@pytest.mark.parametrize('empty_str', [([''], 0.0)])
+def test_empty_list(empty_str):
+    non_char, num_complexifiers = empty_str
     empty_complexifier = compute_complexity(non_char)
     assert empty_complexifier == num_complexifiers
 
 
-@pytest.mark.parametrize('int_and_char', [('abc~@#', 50.0)])
-
-def test_int_and_char(int_and_char):
-    intchar, num_complexifiers = int_and_char
+@pytest.mark.parametrize('letters_and_char', [('abc~@#', 50.0)])
+def test_int_and_char(letters_and_char):
+    intchar, num_complexifiers = letters_and_char
     int_char = compute_complexity(intchar)
     assert int_char == num_complexifiers
 
-@pytest.mark.parametrize('nonchar_and_int', [('abc*(;', 0.0)])
 
-def test_nonchar_and_int(nonchar_and_int):
-    nonchar_int, num_complexifiers = nonchar_and_int
+@pytest.mark.parametrize('nonchar_abc', [('abc*(;', 0.0)])
+def test_nonchar_and_int(nonchar_abc):
+    nonchar_int, num_complexifiers = nonchar_abc
     char_int = compute_complexity(nonchar_int)
     assert char_int == num_complexifiers
-
-
-
-
-
